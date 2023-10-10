0c7dc0b4276e8104a4d088588465a43d28c14a57
Dinissantos <Dinissantos999@hotmail.com>
2023-10-10 16:12:07 +0100

    zombie code removed

--- backend/modules/api/controllers/UserController.php
+++ backend/modules/api/controllers/UserController.php
@@ -8,9 +8,4 @@ use yii\rest\ActiveController;
 class UserController extends ActiveController
 {
     public $modelClass = 'common\models\User';
-
-    public function actionInfo()
-    {
-        echo Yii::$app->Mosquitto->publish();
-    }
 }
