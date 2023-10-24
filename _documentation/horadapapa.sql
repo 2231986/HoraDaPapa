bf94acb51865a9036781f049209e8d6de8937bdd
Dinissantos <Dinissantos999@hotmail.com>
2023-10-24 21:34:34 +0100

    ajuste à tabela 'plate'

--- _documentation/horadapapa.sql
+++ _documentation/horadapapa.sql
@@ -6,50 +6,55 @@
 -- Generation Time: Oct 17, 2023 at 09:13 PM
 -- Server version: 5.7.39
 -- PHP Version: 8.2.0
+SET
+  SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
 
+SET
+  time_zone = "+00:00";
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
+;
+
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
+;
+
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
+;
+
+/*!40101 SET NAMES utf8mb4 */
+;
 
 --
 -- Database: `horadapapa`
 --
-
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `auth_assignment`
 --
-
 CREATE TABLE `auth_assignment` (
   `item_name` varchar(64) NOT NULL,
   `user_id` varchar(64) NOT NULL,
   `created_at` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 --
 -- Dumping data for table `auth_assignment`
 --
-
-INSERT INTO `auth_assignment` (`item_name`, `user_id`, `created_at`) VALUES
-('admin', '1', 1697275759),
-('client', '1', 1697488914),
-('client', '5', 1697306197),
-('cooker', '2', 1697275759),
-('waiter', '3', 1697275759);
+INSERT INTO
+  `auth_assignment` (`item_name`, `user_id`, `created_at`)
+VALUES
+  ('admin', '1', 1697275759),
+  ('client', '1', 1697488914),
+  ('client', '5', 1697306197),
+  ('cooker', '2', 1697275759),
+  ('waiter', '3', 1697275759);
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `auth_item`
 --
-
 CREATE TABLE `auth_item` (
   `name` varchar(64) NOT NULL,
   `type` smallint(6) NOT NULL,
@@ -58,158 +63,194 @@ CREATE TABLE `auth_item` (
   `data` blob,
   `created_at` int(11) DEFAULT NULL,
   `updated_at` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 --
 -- Dumping data for table `auth_item`
 --
-
-INSERT INTO `auth_item` (`name`, `type`, `description`, `rule_name`, `data`, `created_at`, `updated_at`) VALUES
-('admin', 1, NULL, NULL, NULL, 1697275759, 1697275759),
-('client', 1, NULL, NULL, NULL, 1697275759, 1697275759),
-('cooker', 1, NULL, NULL, NULL, 1697275759, 1697275759),
-('managePlate', 2, 'Gere um Prato', NULL, NULL, 1697275759, 1697275759),
-('waiter', 1, NULL, NULL, NULL, 1697275759, 1697275759);
+INSERT INTO
+  `auth_item` (
+    `name`,
+    `type`,
+    `description`,
+    `rule_name`,
+    `data`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'admin',
+    1,
+    NULL,
+    NULL,
+    NULL,
+    1697275759,
+    1697275759
+  ),
+  (
+    'client',
+    1,
+    NULL,
+    NULL,
+    NULL,
+    1697275759,
+    1697275759
+  ),
+  (
+    'cooker',
+    1,
+    NULL,
+    NULL,
+    NULL,
+    1697275759,
+    1697275759
+  ),
+  (
+    'managePlate',
+    2,
+    'Gere um Prato',
+    NULL,
+    NULL,
+    1697275759,
+    1697275759
+  ),
+  (
+    'waiter',
+    1,
+    NULL,
+    NULL,
+    NULL,
+    1697275759,
+    1697275759
+  );
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `auth_item_child`
 --
-
 CREATE TABLE `auth_item_child` (
   `parent` varchar(64) NOT NULL,
   `child` varchar(64) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 --
 -- Dumping data for table `auth_item_child`
 --
-
-INSERT INTO `auth_item_child` (`parent`, `child`) VALUES
-('admin', 'managePlate'),
-('cooker', 'managePlate');
+INSERT INTO
+  `auth_item_child` (`parent`, `child`)
+VALUES
+  ('admin', 'managePlate'),
+  ('cooker', 'managePlate');
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `auth_rule`
 --
-
 CREATE TABLE `auth_rule` (
   `name` varchar(64) NOT NULL,
   `data` blob,
   `created_at` int(11) DEFAULT NULL,
   `updated_at` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `dinner`
 --
-
 CREATE TABLE `dinner` (
   `id` int(11) NOT NULL COMMENT 'id da mesa',
   `name` varchar(255) NOT NULL COMMENT 'nome da mesa',
   `isClean` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'estado da mesa'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `favorite`
 --
-
 CREATE TABLE `favorite` (
   `id` int(11) NOT NULL COMMENT 'id do favorito',
   `plate_id` int(11) NOT NULL COMMENT 'id do prato',
   `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'data',
   `user_id` int(11) NOT NULL COMMENT 'id do utilizador'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `helpticket`
 --
-
 CREATE TABLE `helpticket` (
   `id` int(11) NOT NULL COMMENT 'id do ticket',
   `id_user` int(11) NOT NULL COMMENT 'id utilizador que fez ticket',
   `status` tinyint(1) NOT NULL COMMENT 'Estado da ajuda'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `invoice`
 --
-
 CREATE TABLE `invoice` (
   `id` int(11) NOT NULL COMMENT 'id da fatura',
-  `total` decimal(6,2) NOT NULL COMMENT 'preço final',
+  `total` decimal(6, 2) NOT NULL COMMENT 'preço final',
   `meal_id` int(11) NOT NULL COMMENT 'id da refeição'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `meal`
 --
-
 CREATE TABLE `meal` (
   `id` int(11) NOT NULL COMMENT 'id da refeição',
   `dinner_table_id` int(11) NOT NULL COMMENT 'id da mesa',
   `checkout` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'estado do pagamento',
   `date_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'data de criação'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `migration`
 --
-
 CREATE TABLE `migration` (
   `version` varchar(180) NOT NULL,
   `apply_time` int(11) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE = MyISAM DEFAULT CHARSET = utf8;
 
 --
 -- Dumping data for table `migration`
 --
-
-INSERT INTO `migration` (`version`, `apply_time`) VALUES
-('m000000_000000_base', 1696446333),
-('m130524_201442_init', 1696446341),
-('m190124_110200_add_verification_token_column_to_user_table', 1696446341);
+INSERT INTO
+  `migration` (`version`, `apply_time`)
+VALUES
+  ('m000000_000000_base', 1696446333),
+  ('m130524_201442_init', 1696446341),
+  (
+    'm190124_110200_add_verification_token_column_to_user_table',
+    1696446341
+  );
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `plate`
 --
-
 CREATE TABLE `plate` (
   `id` int(11) NOT NULL COMMENT 'id do prato',
+  `title` varchar(255) NOT NULL COMMENT 'título do prato',
   `description` varchar(255) NOT NULL COMMENT 'descrição do prato',
-  `price` decimal(6,2) NOT NULL COMMENT 'preço do prato'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `price` decimal(6, 2) NOT NULL COMMENT 'preço do prato' `isAvailable` tinyint(1) NOT NULL DEFAULT '1' COMMENT 'disponibilidade do prato'
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 --
 -- Dumping data for table `plate`
 --
-
-INSERT INTO `plate` (`id`, `description`, `price`) VALUES
-(1, 'arroz', '5.00');
+INSERT INTO
+  `plate` (`id`, `description`, `price`)
+VALUES
+  (1, 'arroz', '5.00');
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `request`
 --
-
 CREATE TABLE `request` (
   `id` int(11) NOT NULL COMMENT 'id do pedido',
   `meal_id` int(11) NOT NULL COMMENT 'id da refeição',
@@ -218,14 +259,12 @@ CREATE TABLE `request` (
   `isCooked` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'estado do cozinheiro',
   `isDelivered` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'estado do garçon',
   `user_id` int(11) NOT NULL COMMENT 'id do utilizador'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `user`
 --
-
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
@@ -237,225 +276,336 @@ CREATE TABLE `user` (
   `created_at` int(11) NOT NULL,
   `updated_at` int(11) NOT NULL,
   `verification_token` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 --
 -- Dumping data for table `user`
 --
-
-INSERT INTO `user` (`id`, `username`, `auth_key`, `password_hash`, `password_reset_token`, `email`, `status`, `created_at`, `updated_at`, `verification_token`) VALUES
-(1, 'Dinis', 'UPFEzZ_a0tjVrtelAsjWqVPFHu8lKW3i', '$2y$13$92kaCka.Q5lVo9lUcaaoJ.ONjZu4haHFsNLlfCnCDg/SjfoUNl6Ni', NULL, 'dinis@ipl.com', 10, 1697488914, 1697488914, 'OszSJNjNOTqPmRnwglpt9NOhq2og_GFZ_1697488914');
+INSERT INTO
+  `user` (
+    `id`,
+    `username`,
+    `auth_key`,
+    `password_hash`,
+    `password_reset_token`,
+    `email`,
+    `status`,
+    `created_at`,
+    `updated_at`,
+    `verification_token`
+  )
+VALUES
+  (
+    1,
+    'Dinis',
+    'UPFEzZ_a0tjVrtelAsjWqVPFHu8lKW3i',
+    '$2y$13$92kaCka.Q5lVo9lUcaaoJ.ONjZu4haHFsNLlfCnCDg/SjfoUNl6Ni',
+    NULL,
+    'dinis@ipl.com',
+    10,
+    1697488914,
+    1697488914,
+    'OszSJNjNOTqPmRnwglpt9NOhq2og_GFZ_1697488914'
+  );
 
 --
 -- Indexes for dumped tables
 --
-
 --
 -- Indexes for table `auth_assignment`
 --
-ALTER TABLE `auth_assignment`
-  ADD PRIMARY KEY (`item_name`,`user_id`),
-  ADD KEY `idx-auth_assignment-user_id` (`user_id`);
+ALTER TABLE
+  `auth_assignment`
+ADD
+  PRIMARY KEY (`item_name`, `user_id`),
+ADD
+  KEY `idx-auth_assignment-user_id` (`user_id`);
 
 --
 -- Indexes for table `auth_item`
 --
-ALTER TABLE `auth_item`
-  ADD PRIMARY KEY (`name`),
-  ADD KEY `rule_name` (`rule_name`),
-  ADD KEY `idx-auth_item-type` (`type`);
+ALTER TABLE
+  `auth_item`
+ADD
+  PRIMARY KEY (`name`),
+ADD
+  KEY `rule_name` (`rule_name`),
+ADD
+  KEY `idx-auth_item-type` (`type`);
 
 --
 -- Indexes for table `auth_item_child`
 --
-ALTER TABLE `auth_item_child`
-  ADD PRIMARY KEY (`parent`,`child`),
-  ADD KEY `child` (`child`);
+ALTER TABLE
+  `auth_item_child`
+ADD
+  PRIMARY KEY (`parent`, `child`),
+ADD
+  KEY `child` (`child`);
 
 --
 -- Indexes for table `auth_rule`
 --
-ALTER TABLE `auth_rule`
-  ADD PRIMARY KEY (`name`);
+ALTER TABLE
+  `auth_rule`
+ADD
+  PRIMARY KEY (`name`);
 
 --
 -- Indexes for table `dinner`
 --
-ALTER TABLE `dinner`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE
+  `dinner`
+ADD
+  PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `favorite`
 --
-ALTER TABLE `favorite`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `plate_id` (`plate_id`),
-  ADD KEY `user_id` (`user_id`);
+ALTER TABLE
+  `favorite`
+ADD
+  PRIMARY KEY (`id`),
+ADD
+  KEY `plate_id` (`plate_id`),
+ADD
+  KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `helpticket`
 --
-ALTER TABLE `helpticket`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_user` (`id_user`),
-  ADD KEY `status` (`status`);
+ALTER TABLE
+  `helpticket`
+ADD
+  PRIMARY KEY (`id`),
+ADD
+  KEY `id_user` (`id_user`),
+ADD
+  KEY `status` (`status`);
 
 --
 -- Indexes for table `invoice`
 --
-ALTER TABLE `invoice`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `meal_id` (`meal_id`);
+ALTER TABLE
+  `invoice`
+ADD
+  PRIMARY KEY (`id`),
+ADD
+  KEY `meal_id` (`meal_id`);
 
 --
 -- Indexes for table `meal`
 --
-ALTER TABLE `meal`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `dinner_table_id` (`dinner_table_id`),
-  ADD KEY `checkout` (`checkout`);
+ALTER TABLE
+  `meal`
+ADD
+  PRIMARY KEY (`id`),
+ADD
+  KEY `dinner_table_id` (`dinner_table_id`),
+ADD
+  KEY `checkout` (`checkout`);
 
 --
 -- Indexes for table `migration`
 --
-ALTER TABLE `migration`
-  ADD PRIMARY KEY (`version`);
+ALTER TABLE
+  `migration`
+ADD
+  PRIMARY KEY (`version`);
 
 --
 -- Indexes for table `plate`
 --
-ALTER TABLE `plate`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE
+  `plate`
+ADD
+  PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `request`
 --
-ALTER TABLE `request`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `meal_id` (`meal_id`),
-  ADD KEY `plate_id` (`plate_id`),
-  ADD KEY `isCooked` (`isCooked`),
-  ADD KEY `isDelivered` (`isDelivered`),
-  ADD KEY `user_id` (`user_id`);
+ALTER TABLE
+  `request`
+ADD
+  PRIMARY KEY (`id`),
+ADD
+  KEY `meal_id` (`meal_id`),
+ADD
+  KEY `plate_id` (`plate_id`),
+ADD
+  KEY `isCooked` (`isCooked`),
+ADD
+  KEY `isDelivered` (`isDelivered`),
+ADD
+  KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `user`
 --
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD UNIQUE KEY `password_reset_token` (`password_reset_token`);
+ALTER TABLE
+  `user`
+ADD
+  PRIMARY KEY (`id`),
+ADD
+  UNIQUE KEY `username` (`username`),
+ADD
+  UNIQUE KEY `email` (`email`),
+ADD
+  UNIQUE KEY `password_reset_token` (`password_reset_token`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
-
 --
 -- AUTO_INCREMENT for table `dinner`
 --
-ALTER TABLE `dinner`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id da mesa';
+ALTER TABLE
+  `dinner`
+MODIFY
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id da mesa';
 
 --
 -- AUTO_INCREMENT for table `favorite`
 --
-ALTER TABLE `favorite`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id do favorito';
+ALTER TABLE
+  `favorite`
+MODIFY
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id do favorito';
 
 --
 -- AUTO_INCREMENT for table `helpticket`
 --
-ALTER TABLE `helpticket`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id do ticket';
+ALTER TABLE
+  `helpticket`
+MODIFY
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id do ticket';
 
 --
 -- AUTO_INCREMENT for table `invoice`
 --
-ALTER TABLE `invoice`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id da fatura';
+ALTER TABLE
+  `invoice`
+MODIFY
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id da fatura';
 
 --
 -- AUTO_INCREMENT for table `meal`
 --
-ALTER TABLE `meal`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id da refeição';
+ALTER TABLE
+  `meal`
+MODIFY
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id da refeição';
 
 --
 -- AUTO_INCREMENT for table `plate`
 --
-ALTER TABLE `plate`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id do prato', AUTO_INCREMENT=2;
+ALTER TABLE
+  `plate`
+MODIFY
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id do prato',
+  AUTO_INCREMENT = 2;
 
 --
 -- AUTO_INCREMENT for table `request`
 --
-ALTER TABLE `request`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id do pedido';
+ALTER TABLE
+  `request`
+MODIFY
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id do pedido';
 
 --
 -- AUTO_INCREMENT for table `user`
 --
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE
+  `user`
+MODIFY
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  AUTO_INCREMENT = 2;
 
 --
 -- Constraints for dumped tables
 --
-
 --
 -- Constraints for table `auth_assignment`
 --
-ALTER TABLE `auth_assignment`
-  ADD CONSTRAINT `auth_assignment_ibfk_1` FOREIGN KEY (`item_name`) REFERENCES `auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE
+  `auth_assignment`
+ADD
+  CONSTRAINT `auth_assignment_ibfk_1` FOREIGN KEY (`item_name`) REFERENCES `auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `auth_item`
 --
-ALTER TABLE `auth_item`
-  ADD CONSTRAINT `auth_item_ibfk_1` FOREIGN KEY (`rule_name`) REFERENCES `auth_rule` (`name`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE
+  `auth_item`
+ADD
+  CONSTRAINT `auth_item_ibfk_1` FOREIGN KEY (`rule_name`) REFERENCES `auth_rule` (`name`) ON DELETE
+SET
+  NULL ON UPDATE CASCADE;
 
 --
 -- Constraints for table `auth_item_child`
 --
-ALTER TABLE `auth_item_child`
-  ADD CONSTRAINT `auth_item_child_ibfk_1` FOREIGN KEY (`parent`) REFERENCES `auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `auth_item_child_ibfk_2` FOREIGN KEY (`child`) REFERENCES `auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE
+  `auth_item_child`
+ADD
+  CONSTRAINT `auth_item_child_ibfk_1` FOREIGN KEY (`parent`) REFERENCES `auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD
+  CONSTRAINT `auth_item_child_ibfk_2` FOREIGN KEY (`child`) REFERENCES `auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `favorite`
 --
-ALTER TABLE `favorite`
-  ADD CONSTRAINT `fk_favorite_plate_id` FOREIGN KEY (`plate_id`) REFERENCES `plate` (`id`),
-  ADD CONSTRAINT `fk_favorite_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+ALTER TABLE
+  `favorite`
+ADD
+  CONSTRAINT `fk_favorite_plate_id` FOREIGN KEY (`plate_id`) REFERENCES `plate` (`id`),
+ADD
+  CONSTRAINT `fk_favorite_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
 --
 -- Constraints for table `helpticket`
 --
-ALTER TABLE `helpticket`
-  ADD CONSTRAINT `FK_helpticket_userID` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
+ALTER TABLE
+  `helpticket`
+ADD
+  CONSTRAINT `FK_helpticket_userID` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
 
 --
 -- Constraints for table `invoice`
 --
-ALTER TABLE `invoice`
-  ADD CONSTRAINT `fk_meal` FOREIGN KEY (`meal_id`) REFERENCES `meal` (`id`);
+ALTER TABLE
+  `invoice`
+ADD
+  CONSTRAINT `fk_meal` FOREIGN KEY (`meal_id`) REFERENCES `meal` (`id`);
 
 --
 -- Constraints for table `meal`
 --
-ALTER TABLE `meal`
-  ADD CONSTRAINT `fk_dinner_table` FOREIGN KEY (`dinner_table_id`) REFERENCES `dinner` (`id`);
+ALTER TABLE
+  `meal`
+ADD
+  CONSTRAINT `fk_dinner_table` FOREIGN KEY (`dinner_table_id`) REFERENCES `dinner` (`id`);
 
 --
 -- Constraints for table `request`
 --
-ALTER TABLE `request`
-  ADD CONSTRAINT `fk_meal_id` FOREIGN KEY (`meal_id`) REFERENCES `meal` (`id`),
-  ADD CONSTRAINT `fk_plate_id` FOREIGN KEY (`plate_id`) REFERENCES `plate` (`id`),
-  ADD CONSTRAINT `fk_request_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+ALTER TABLE
+  `request`
+ADD
+  CONSTRAINT `fk_meal_id` FOREIGN KEY (`meal_id`) REFERENCES `meal` (`id`),
+ADD
+  CONSTRAINT `fk_plate_id` FOREIGN KEY (`plate_id`) REFERENCES `plate` (`id`),
+ADD
+  CONSTRAINT `fk_request_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
+;
+
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
+;
+
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
+;
