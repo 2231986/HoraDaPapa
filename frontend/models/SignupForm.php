c7f7c4bf86bb5d2c30beea5646f3a66865c87a54
Rebelo-ipl <2211914@my.ipleiria.pt>
2023-11-07 20:08:24 +0000

    Fix Registo Utilizador
    Guardar campo NIF

--- frontend/models/SignupForm.php
+++ frontend/models/SignupForm.php
@@ -76,6 +76,7 @@ class SignupForm extends Model
         $userinfo = new UserInfo();
         $userinfo->name = $this->name;
         $userinfo->surname = $this->surname;
+        $userinfo->nif = $this->nif;
         $userinfo->user_id = $user->id;
         $userinfo->save();
 
