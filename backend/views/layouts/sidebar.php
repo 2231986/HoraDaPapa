4d3dc8ce437cf3def9390e42d7755ec1e5f0e2ef
Dinissantos <Dinissantos999@hotmail.com>
2023-12-13 19:54:31 +0000

    logo do projeto no backend

--- backend/views/layouts/sidebar.php
+++ backend/views/layouts/sidebar.php
@@ -77,7 +77,7 @@ if (Yii::$app->user->can(RbacController::$PermissionMeal))
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="<?= Url::toRoute(['site/index']) ?>" class="brand-link">
-        <img src="<?= $assetDir ?>/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="<?= Yii::getAlias('@web/images/logo.png') ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Hora da Papa</span>
     </a>
 
