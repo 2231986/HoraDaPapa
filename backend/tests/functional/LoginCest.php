de8b97cb677aa5350ad5b4b2b41e37dcd10fd1a3
Dinissantos <Dinissantos999@hotmail.com>
2023-12-18 21:49:52 +0000

    testes base de backend

--- backend/tests/functional/LoginCest.php
+++ backend/tests/functional/LoginCest.php
@@ -4,6 +4,8 @@ namespace backend\tests\functional;
 
 use backend\tests\FunctionalTester;
 use common\fixtures\UserFixture;
+use common\models\User;
+use console\controllers\RbacController;
 
 /**
  * Class LoginCest
@@ -27,17 +29,84 @@ class LoginCest
         ];
     }
 
-    /**
-     * @param FunctionalTester $I
-     */
-    public function loginUser(FunctionalTester $I)
+    public function _before(FunctionalTester $I)
+    {
+        $user = User::find()->where(['id' => 1])->one();
+
+        $auth = \Yii::$app->authManager;
+        $role = $auth->getRole(RbacController::$RoleAdmin);
+        $auth->assign($role,  $user->id);
+
+        $user = User::find()->where(['id' => 2])->one();
+
+        $auth = \Yii::$app->authManager;
+        $role = $auth->getRole(RbacController::$RoleCooker);
+        $auth->assign($role,  $user->id);
+
+        $user = User::find()->where(['id' => 3])->one();
+
+        $auth = \Yii::$app->authManager;
+        $role = $auth->getRole(RbacController::$RoleWaiter);
+        $auth->assign($role,  $user->id);
+
+        $user = User::find()->where(['id' => 4])->one();
+
+        $auth = \Yii::$app->authManager;
+        $role = $auth->getRole(RbacController::$RoleClient);
+        $auth->assign($role,  $user->id);
+
+        $I->amOnRoute('site/login');
+    }
+
+    public function loginAdmin(FunctionalTester $I)
     {
         $I->amOnRoute('/site/login');
+        $I->see('Sign in to start your session', 'p');
+
         $I->fillField('#loginform-username', 'admin');
         $I->fillField('#loginform-password', '12345678');
-        $I->click('Sign In');
+        $I->click('Sign In', 'form button[type=submit]');
 
-        $I->see('Página Principal');
-        $I->dontSeeLink('Sign In');
+        $I->see('Página Principal', 'h1');
+        $I->dontSeeLink('Sign In', 'form button[type=submit]');
+    }
+
+    public function loginCooker(FunctionalTester $I)
+    {
+        $I->amOnRoute('/site/login');
+        $I->see('Sign in to start your session', 'p');
+
+        $I->fillField('#loginform-username', 'cooker');
+        $I->fillField('#loginform-password', '12345678');
+        $I->click('Sign In', 'form button[type=submit]');
+
+        $I->see('Página Principal', 'h1');
+        $I->dontSeeLink('Sign In', 'form button[type=submit]');
+    }
+
+    public function loginWaiter(FunctionalTester $I)
+    {
+        $I->amOnRoute('/site/login');
+        $I->see('Sign in to start your session', 'p');
+
+        $I->fillField('#loginform-username', 'waiter');
+        $I->fillField('#loginform-password', '12345678');
+        $I->click('Sign In', 'form button[type=submit]');
+
+        $I->see('Página Principal', 'h1');
+        $I->dontSeeLink('Sign In', 'form button[type=submit]');
+    }
+
+    public function loginClient(FunctionalTester $I) //Não é suposto conseguir fazer login
+    {
+        $I->amOnRoute('/site/login');
+        $I->see('Sign in to start your session', 'p');
+
+        $I->fillField('#loginform-username', 'client');
+        $I->fillField('#loginform-password', '12345678');
+        $I->click('Sign In', 'form button[type=submit]');
+
+        $I->dontSeeLink('Página Principal', 'h1');
+        $I->see('Sign in to start your session', 'p');
     }
 }
